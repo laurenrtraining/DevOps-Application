7a2d2ae006a5c628bd427ba29b4392c4a7634dfb
Lauren <152506260+laurenrtraining@users.noreply.github.com>
2026-03-15 18:36:17 +0000

    Trying to get SMTP to work when deployed

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ mail = Mail()
 
 
 def init_mail(app):
-    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_SERVER"] = "smtp-relay.gmail.com"
     app.config["MAIL_PORT"] = 587
     app.config["MAIL_USE_SSL"] = False
     app.config["MAIL_USE_TLS"] = True
